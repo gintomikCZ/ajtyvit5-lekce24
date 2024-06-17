3dd1d61d89d1150de97bbac60951c42b89fb2a81
Tomas Glabazna <tomas@glabazna.cz>
2024-06-17 19:21:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+<!-- primary color of the project is lightgreen -->
+
+  <h1>lekce 24</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam tempora maiores perferendis molestias asperiores quidem ut explicabo aspernatur, iste maxime assumenda sint amet voluptas repellendus voluptatum quae consectetur eius necessitatibus.</p>
+  <TButton label="click me!" />
+</template>
+
+<script>
+
+import TButton from '@/components/TButton.vue'
+export default {
+  name: 'App',
+  components: { TButton }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+@import './styles/variables.styl'
+h1
+  border-bottom: 2px solid $primary
+  margin-bottom: 1rem
+
+</style>
---
+++ src/components/TButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="$emit('clicked')">{{ label }}</button>
+</template>
+
+<script>
+
+export default {
+  name: 'TButton',
+  props: {
+    label: String
+  }
+}
+
+</script>
+<style lang="stylus" scoped>
+@import '../styles/variables.styl'
+
+button
+  font-size: 1.4rem
+  padding: .4em .8em
+  border-radius: 5px
+  border: 2px solid #222
+  if dark($primary)
+    color: white
+  else
+    color: #222
+  background: $primary
+  cursor: pointer
+  transition: background-color .4s ease
+  &:hover
+    if dark($primary)
+      background: darken($primary, 40%)
+    else
+      background: lighten($primary, 40%)
+
+</style>
